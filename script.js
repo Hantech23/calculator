87cff5ed968111c0c12eba63450b9df156f9c9d6
Woobin Han <woobin.han23@gmail.com>
2022-04-18 05:06:21 -0400

    Finish button design

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-const buttons = document.querySelector('.buttons');
-
-for(i = 0; i < 19; i++) {
-    const e = document.createElement('button');
-    buttons.append(e);
-    if (i == 16){
-        e.setAttribute("style", "grid-column: span 2");
-    }
-}
 
