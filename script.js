7647b9182ea83f456366b9310012a5ef1de14e80
Woobin Han <woobin.han23@gmail.com>
2022-04-24 00:55:24 -0400

    Finish number buttons, operator buttons, and clear button

--- script.js
+++ script.js
@@ -1,31 +1,34 @@
+//OPERATIONS
 const add = function(a,b) {
-	return a + b;
+	return +a + +b;
 };
-
 const subtract = function(a,b) {
-	return a - b;
+	return +a - +b;
 };
-
-const sum = function(array) {
-	return array.reduce((a,b) => a + b, 0);
+const multiply = function(a,b) {
+  return +a * +b;
 };
-
-const multiply = function(array) {
-  return array.reduce((a,b) => a * b, 1)
-};
-
 const divide = function(a,b) {
-    return a/b;
-}
-/*
-function operate(num1, operator, num2) {
-    return operator(a,b);
-}
-*/
-
+    return +a/+b;
+};
+const percent = function(x) {
+    return +x/100;
+};
+const changeSign = function(x) {
+    return +x * -1;
+};
+//DISPLAY
 const display = document.querySelector('.screen')
 display.textContent = '0';
 
+const clear = document.querySelector('.ac')
+clear.addEventListener("click", () => {
+    num1 = 0;
+    num2 = 0;
+    operator = undefined;
+    display.textContent = '0';
+})
+//NUMBERS
 const zero = document.querySelector('.zero')
 const one = document.querySelector('.one')
 const two = document.querySelector('.two')
@@ -36,82 +39,167 @@ const six = document.querySelector('.six')
 const seven = document.querySelector('.seven')
 const eight = document.querySelector('.eight')
 const nine = document.querySelector('.nine')
-
+//OPERATORS
+const addition = document.querySelector('.add')
+const subtraction = document.querySelector('.subtract')
+const multiplication = document.querySelector('.multiply')
+const division = document.querySelector('.divide')
+const equals = document.querySelector('.equals')
+const percentage = document.querySelector('.percent')
+const plusOrMinus = document.querySelector('.change-sign')
+//VARIABLES
 let num1 = 0;
+let num2 = 0;
 let operator;
-console.log(operator)
-
+//EVENTLISTENERS FOR NUMBERS
 zero.addEventListener("click", () => {
-    firstValue('0')
+    numberInput('0')
 })
 one.addEventListener("click", () => {
-    firstValue('1')
+    numberInput('1')
 })
 two.addEventListener("click", () => {
-    firstValue('2')
+    numberInput('2')
 })
 three.addEventListener("click", () => {
-    firstValue('3')
+    numberInput('3')
 })
 four.addEventListener("click", () => {
-    firstValue('4')
+    numberInput('4')
 })
 five.addEventListener("click", () => {
-    firstValue('5')
+    numberInput('5')
 })
 six.addEventListener("click", () => {
-    firstValue('6')
+    numberInput('6')
 })
 seven.addEventListener("click", () => {
-    firstValue('7')
+    numberInput('7')
 })
 eight.addEventListener("click", () => {
-    firstValue('8')
+    numberInput('8')
 })
 nine.addEventListener("click", () => {
-    firstValue('9')
+    numberInput('9')
+})
+//EVENT LISTENERS FOR OPERATORS
+addition.addEventListener("click", () => {
+    operatorInput('+')
+    console.log(operator)
+})
+subtraction.addEventListener("click", () => {
+    operatorInput('-')
+    console.log(operator)
+})
+multiplication.addEventListener("click", () => {
+    operatorInput('*')
+    console.log(operator)
+})
+division.addEventListener("click", () => {
+    operatorInput('/')
+    console.log(operator)
+})
+equals.addEventListener("click", () => {
+    if (!(operator == undefined)) {
+        calculate (operator);
+    }
+})
+percentage.addEventListener("click", () => {
+    percentORplusminus('%')
+})
+plusOrMinus.addEventListener("click", () => {
+    percentORplusminus('+/-')
 })
 
 
-function firstValue (input) {
-    if (num1 == 0) {
-        num1 = input;
-        display.textContent = input;
-        console.log(num1)
+function numberInput (input) {
+    if (operator == undefined) {
+        if (num1 == 0) {
+            num1 = input;
+            display.textContent = input;
+            console.log('a = ' + num1)
+        }
+        else {
+            num1 += input;
+            display.textContent = num1;
+            console.log('a = ' + num1)
+        }
     }
     else {
-        num1 += input;
+        if (num2 == 0) {
+            num2 = input;
+            display.textContent = input;
+            console.log('b = ' + num2)
+        }
+        else {
+            num2 += input;
+            display.textContent = num2;
+            console.log('b = ' + num2)
+        }
+    }
+        
+}
+
+function calculate (operator) {
+    if (operator == '+') {
+        num1 = add(num1,num2);
+        num2 = 0;
         display.textContent = num1;
-        console.log(num1)
+    }
+    else if (operator == '-') {
+        num1 = subtract(num1,num2);
+        num2 = 0;
+        display.textContent = num1;
+    }
+    else if (operator == '*') {
+        num1 = multiply(num1,num2);
+        num2 = 0;
+        display.textContent = num1;
+    }
+    else if (operator == '/') {
+        num1 = divide(num1,num2);
+        num2 = 0;
+        display.textContent = num1;
     }
 }
 
-//num1 = 50
-//firstValue('3')
-/*
-console.log(num1)
-num1 += 1;
-console.log(num1)
-
-num1 += '20';
-console.log(num1)
-*/
-
-
-//num1 = 10;
-//display.textContent = num1; 
-/*
-one.addEventListener("click", () => {
-    num1 += '1';
-    display.textContent = num1;
-})
-*/
-
-
-/*
-function operate(key) {
-   if (num1 = 0) {
-       return num1 = key;
-   } 
+function operatorInput (input) {
+    if (operator == undefined) {
+        operator = input;
+    }
+    else {
+        calculate (operator);
+        operator = input;
+    }
 }
+
+/*  SIGNIFICANT BUG:
+    Pressing these buttons immediately after a result is displayed will cause
+    the display to display 0 because num2 is set to 0 after every operation.
 */
+function percentORplusminus (eitherButton) {
+    if (operator == undefined) {
+        if (eitherButton == '%') {
+            num1 = percent(num1);
+            display.textContent = num1;
+            console.log('a = ' + num1);
+        }
+        else if (eitherButton == '+/-') {
+            num1 = changeSign(num1);
+            display.textContent = num1;
+            console.log('a = ' + num1);
+        }
+    }
+    else {
+        if (eitherButton == '%') {
+            num2 = percent(num2);
+            display.textContent = num2;
+            console.log('b = ' + num2);
+        }
+        else if (eitherButton == '+/-') {
+            num2 = changeSign(num2);
+            display.textContent = num2;
+            console.log('b = ' + num2);
+        }
+    }
+}
